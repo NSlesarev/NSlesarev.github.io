2c32db17f1d4f89da320e896c2215f56fca240b2
admin <admin@iMac-admin.local>
2024-03-27 19:46:29 +0300

    добавил открвание и закрывание задач на стрелочку, так же добавил отображение тру или фолс на чекбоксах в локал стороже

--- js/main.js
+++ js/main.js
@@ -1,65 +1,17 @@
 
-// document.addEventListener("DOMContentLoaded", function() {
-
-//   const inputTasks = document.getElementById("inputTasks");
-//   const buttonAdd = document.getElementById("buttonAdd");
-//   const todoList = document.getElementById(".content-items");
-//   const filterOptions = document.getElementById(".content-option");
-
-//   // Массив для хранения задач
-//   let tasks = [];
-
-//   // Функция для отображения задач из массива
-//   function renderTodoList() {
-    
-//       todoList.innerHTML = ''; // Очищаем список
-  
-      
-
-//       tasks.forEach(function(task) {
-//           let li = document.createElement('li');
-//           li.textContent = task;
-//           todoList.appendChild(li);
-//       });
-//   }
-
-//   // Функция для добавления новой задачи
-//   function addTask() {
-//       let taskText = inputTasks.value.trim();
-//       if (taskText !== '') {
-//           tasks.push(taskText);
-//           inputTasks.value = ''; // Очищаем поле ввода
-//           renderTodoList(); // Перерисовываем список задач
-//       }
-//   }
-
-//   // Обработчик нажатия на кнопку "Добавить задачу"
-//   buttonAdd.addEventListener("click", addTask);
-
-//   // Обработчик нажатия на Enter в поле ввода
-//   inputTasks.addEventListener("keypress", function(event) {
-//       if (event.key === "Enter") {
-//           addTask();
-//       }
-//   });
-
-//   // Обработчик изменения фильтра задач
-//   filterOptions.addEventListener("change", function() {
-//       let filterValue = filterOptions.value;
-//   });
-// });
-
-
-
-
-
-
-// document.addEventListener('DOMContentLoaded', function() {})
 
   let inputTasks = document.querySelector('#inputTasks'); //Инпут для ввода задач
   let buttonAdd = document.querySelector('#buttonAdd'); // кнопка Добавить Задачу
   let iconArrow = document.querySelector('.icon');  // будущая кнопка стрелка
-  let contentItems = document.querySelector('.content-items'); //переменная куда будет появлятся задача
+  let contentItems = document.querySelector('.content-items'); //переменная куда будет появлятся задача  
+
+  let all = document.querySelectorAll('#all');
+  let active = document.querySelectorAll('#active');
+  let completed = document.querySelectorAll('#completed');
+  let clearCompleted = document.querySelectorAll('#clearCompleted');
+
+
+
 
 
   
@@ -80,40 +32,92 @@
 
   //обработчик события на кнопке Добавить задачу
   buttonAdd.addEventListener('click', function() {
-
+    
 
     //обьект с данными о задачах
     let obj = {
       contentItems: inputTasks.value,
-      checked: false,
+      checked: false
     };
 
     // добавляем обьекты в массив
-    tasks.push(obj);
+    tasks.unshift(obj);
     displayMessages()
     // сохранение данных, и через JSON преобразуем в строки
     localStorage.setItem('contentItems', JSON.stringify(tasks));
+    
 
   });
     //Создаем функцию по добавлению новых окон
   function displayMessages() {
+    
     // сама переменная останется пустой, так как подписи задач не нужно
     let displayMessage = '';
     
     
     
     tasks.forEach(function(item, i) {
+      
       //добавления тега li через += для того что бы они поступали столько сколько это необходимо
      displayMessage += `
-      <li>
-        <input class="check" type="checkbox" id='item_${i}' ${item.checked ? checked : ''}>
-        <label class="lable-check" for='item_${i}'></label>
+      <li class="box">
+        <input class="check" type="checkbox" id='${i}' ${item.checked ? 'checked' : ''}>
         <p class="text" id="item_${i}">${item.contentItems}</p>
+        <label class="lable-check" for='item_${i}'></label>
       </li>
      `;
      //реализуем появление на сайте
      contentItems.innerHTML = displayMessage;
     });
-  }
+  };
 
+  
+  
+  let boxes = document.querySelectorAll('.box');
+    
+  iconArrow.addEventListener('click', function() {
+   
+      boxes.forEach(function(box) {
+        box.toggleAttribute('hidden');
+      });
+  });
+
+  let checkBox = document.querySelectorAll('.check');
+
+    checkBox.forEach(function(item){
+      item.addEventListener('click', function(){
+        tasks[item.id].checked = !tasks[item.id].checked;
+        console.log( tasks[item.id])
+        localStorage.setItem('contentItems', JSON.stringify(tasks));
+      })
+    });
+    
+    
+    
+    completed.addEventListener('click', function(){
+      
+      let completedTasks = tasks.filter(function(task) {
+        return task.checked;
+        
+        
+      });
+      displayCompletedTasks(completedTasks);
+    })
+
+  
+
+  // contentItems.addEventListener('change', function(event){
+  //   let idInput = event.target.getAttribute('id');
+  //   let forLabel = contentItems.querySelector('[for=' + idInput + ']');
+  //   let valueLable = forLabel.innerHTML;
+    
+  //   contentItems.forEach(function(item){
+  //     if (item.contentItems === valueLable) {
+  //       item.checked = !item.checked;
+  //       localStorage.setItem('contentItems', JSON.stringify(tasks));
+  //     }
+  //   });
+  // });
+
+  
   
