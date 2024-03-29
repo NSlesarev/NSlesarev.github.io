da42802a0ab9e20979c49a8446d1e10c1bb1a31e
admin <admin@iMac-admin.local>
2024-03-29 18:17:50 +0300

    сделал кнопки сортировки задач, так же кнопку которая удаляет все задачи разом,  и сделал фильтр по чекбоксу в разных состояниях

--- js/main.js
+++ js/main.js
@@ -3,27 +3,28 @@ let buttonAdd = document.querySelector("#buttonAdd"); // кнопка Добав
 let iconArrow = document.querySelector(".icon"); // будущая кнопка стрелка
 let contentItems = document.querySelector(".content-items"); //переменная куда будет появлятся задача
 
-let all = document.querySelectorAll("#all");
-let active = document.querySelectorAll("#active");
-let completed = document.querySelectorAll(".completed");
-let clearCompleted = document.querySelectorAll("#clearCompleted");
+let all = document.querySelector("#all");
+let active = document.querySelector("#active");
+let completed = document.querySelector("#completed");
+let clearCompleted = document.querySelector("#clearComplited");
 let deletedIcon = document.querySelector('.deleted');
 let counter = document.querySelector('.content-num');
-let deletedAll = document.querySelector('.content-deletAll')
+let deletedAll = document.querySelector('.content-deletAll');
+let deletedAlll = document.querySelector('.cont-deletAll');
+let content = document.querySelector('.content-ul');
 //Массив для хранения задач
-let tasks = [];
+let tasks = localStorage.getItem("contentItems") ? JSON.parse(localStorage.getItem("contentItems")) : [];
 
-//получаем данные которые были ранее написаны в Tasks(массив)
-if (localStorage.getItem("contentItems")) {
-  tasks = JSON.parse(localStorage.getItem("contentItems"));
+let currentTab = 'all';
 
-  displayMessages();
-}
+
+displayMessages();
 
 //обработчик события на кнопке Добавить задачу
 buttonAdd.addEventListener("click", function () {
   //обьект с данными о задачах
   let obj = {
+    id: Date.now(),
     contentItems: inputTasks.value,
     checked: false,
   };
@@ -33,28 +34,99 @@ buttonAdd.addEventListener("click", function () {
   displayMessages();
   inputTasks.value = "";
   // сохранение данных, и через JSON преобразуем в строки
-  localStorage.setItem("contentItems", JSON.stringify(tasks));
 });
+
+
 //Создаем функцию по добавлению новых окон
 function displayMessages() {
+  // console.log(tasks)
   // сама переменная останется пустой, так как подписи задач не нужно
   let displayMessage = "";
+  
+  let renderedTasks = []
 
-  tasks.forEach(function (item, i) {
+  switch (currentTab) {
+    
+
+    case 'active': {
+      renderedTasks = tasks.filter(function(item) {
+        return item.checked === false;
+        
+      })
+      break;
+    } 
+
+    case 'completed': {
+      renderedTasks = tasks.filter(function(item) {
+        return item.checked === true;
+      })
+      break;
+    }
+
+
+    default: {
+      renderedTasks = tasks;
+      break;
+    }
+  }
+  
+  
+//прописать каждое условие на каждое значение Таба, 
+
+// обратиться к группе, и через фореач накинуть на каждый элемент обработчик событий, и затем в зависимости от переменной каренТаб менять стили в дереве для активного таба
+
+renderedTasks.forEach(function (item, i) {
     //добавления тега li через += для того что бы они поступали столько сколько это необходимо
     displayMessage += `
       <li class="box">
-        <input class="check" type="checkbox" id='${i}' ${item.checked ? "checked" : ""}>
+        <input class="check" type="checkbox" id='${item.id}' ${item.checked ? "checked" : ""}>
         <p class="text" id="item_${i}">${item.contentItems}</p>
         <label class="lable-check" for='item_${i}'></label>
         <img class='deleted' src='../src/img/deleted.png'>
       </li>
      `;
     //реализуем появление на сайте
-    contentItems.innerHTML = displayMessage;
   });
+
+  // console.log(displayMessage)
+
+  contentItems.innerHTML = displayMessage;
+
   counter.textContent = `${tasks.length} items`;
+
+  localStorage.setItem("contentItems", JSON.stringify(tasks));
 }
+
+all.addEventListener('click', function(){
+  currentTab = 'all';
+  displayMessages();
+})
+
+
+active.addEventListener('click', function(){
+  currentTab = 'active';
+  
+  displayMessages();
+})
+
+
+completed.addEventListener('click', function(){
+  currentTab = 'completed';
+  displayMessages();
+})
+
+
+
+clearCompleted.addEventListener('click', function(){
+  tasks = tasks.filter(item =>{
+    return item.checked === false;
+  })
+
+  displayMessages()
+})
+
+
+
 
 let boxes = document.querySelectorAll(".box");
 
@@ -72,35 +144,23 @@ iconArrow.addEventListener("click", function () {
 
 let checkBox = document.querySelectorAll(".check");
 
-checkBox.forEach(function (item) {
-  item.addEventListener("click", function () {
+
+checkBox.forEach(function(item) {
+
+  item.addEventListener("change", function () {
+
     tasks[item.id].checked = !tasks[item.id].checked;
+
     counter.textContent = `${tasks.length} items`;
 
-    localStorage.setItem("contentItems", JSON.stringify(tasks));
+    
+    displayMessages();
   });
+
 });
-// function arraySorting(tasks) {
-//   const sort = tasks.sort((a, b) =>
-//     a.checked === b.checked ? 0 : a.checked ? -1 : 1
-//   );
-//   return sort;
-// }
 
-// completed.addEventListener("click", function () {
-  
-//   const completedTasks = sortedTasks.filter((task) => task.checked === true);
 
-//   contentItems.innerHTML = "";
 
-//   completedTasks.forEach((task) => {
-//     const taskItem = document.createElement("li");
-
-//     taskItem.textContent = task.contentItems;
-
-//     contentItems.appendChild(taskItem);
-//   });
-// });
 contentItems.addEventListener("click", function(event) {
 
 
@@ -113,32 +173,17 @@ contentItems.addEventListener("click", function(event) {
     tasks.splice(index, 1).requestAnimationFrame; 
 
     displayMessages(); 
-
-
-
-    
-    localStorage.setItem("contentItems", JSON.stringify(tasks));
   }
 });
 
-contentItems.addEventListener("click", function(event) {
-
-  if (event.target.classList.contains('deletedAll')) {
-
-    const li = event.target.closest('.content-deletAll');
-
-    const ind = Array.from(contentItems.children).indexOf(li);
-
-    tasks.splice(ind, tasks.length - ind);
 
 
-    
-    displayMessages();
-    
 
+deletedAll.addEventListener("click", function() {
+  tasks = [];
 
-    
-    localStorage.setItem("contentItems", JSON.stringify(tasks));
-
-  }
+  displayMessages();
+      
 });
+
+
