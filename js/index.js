36abdbafb573f48d7f40d908767aaf959648f23d
admin <admin@iMac-admin.local>
2024-04-15 18:26:14 +0300

    собрал билд

--- js/index.js
+++
@@ -1,8 +0,0 @@
-import './renderTasks.js';
-import './filterTasksAndDeletedTasks.js';
-import './showsAndHidesTheTask.js';
-import './buttonDeleted.js';
-import './checkBox.js';
-import './changeAllCheckboxes.js';
-import './buttonAddTasks.js';
-import './selectors.js';
--- js/showsAndHidesTheTask.js
+++
@@ -1,9 +0,0 @@
-import { iconArrow } from './selectors';
-
-iconArrow.addEventListener('click', function () {
-	let boxes = document.querySelectorAll('.box');
-	boxes.forEach(function (box) {
-		box.classList.toggle('hidden');
-	});
-	iconArrow.classList.toggle('rotate');
-});
